5e37941e87ed69d6a7b57b644988595a20b3ab38
Rodrigo Cuadra <69258390+rodrigoCuadraCaro@users.noreply.github.com>
2022-01-23 23:53:06 -0300

    Added encryption and decrypt functions

--- JS/func.js
+++ JS/func.js
@@ -1,15 +1,24 @@
+//-------------- Encryption and Decryption keys ------------ //
 const encrypter = {'e':'enter', 'i':'imes','a':'ai', 'o':'ober', 'u':'ufat'};
 const decrypter = {'enter':'e', 'imes':'i','ai':'a', 'ober':'o', 'ufat':'u'};
+//----------------------------------------------------------//
+
+//This function encrypts the word only if there is no uppercase letters
+function encryptWord(){
+    let text = document.getElementById("txtEnc").value;
+    if (!/[A-Z]/g.test(text)){
+        document.getElementById("texto").innerHTML=
+            text.replace(/[aeiou]/g, toReplace => encrypter[toReplace]);
+    } else{
+        alert("Recuerde que el texto solo deben ser minúsculas!")
+    }
+
+}
 
 
-let word = 'gato';
-
-word = word.replace(/[aeiou]/g, toReplace => encrypter[toReplace]);
-
-console.log(word);
-
-word = word.replace(/enter|imes|ai|ober|ufat/g, toReplace => decrypter[toReplace]);
-
-console.log(word);
-
-
+function decryptWord(){
+    let text = document.getElementById("txtEnc").value;
+    console.log(text);
+    document.getElementById("texto").innerHTML =
+        text.replace(/enter|imes|ai|ober|ufat/g, toReplace => decrypter[toReplace]);
+}
